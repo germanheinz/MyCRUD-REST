73a859d4dd7a26c6915cfd0c96ebc6c7a841d13a
german <heinz.german@gmail.com>
2020-05-13 15:42:49 +0200

    Model

---
+++ lib/src/models/product.dart
@@ -0,0 +1,38 @@
+
+import 'dart:convert';
+
+ProducModel producModelFromJson(String str) => ProducModel.fromJson(json.decode(str));
+
+String producModelToJson(ProducModel data) => json.encode(data.toJson());
+
+class ProducModel {
+    int id;
+    String title;
+    double value;
+    bool stock;
+    String photoUrl;
+
+    ProducModel({
+        this.id,
+        this.title  = '',
+        this.value  = 0.0,
+        this.stock  = true,
+        this.photoUrl,
+    });
+
+    factory ProducModel.fromJson(Map<String, dynamic> json) => ProducModel(
+        id      : json["id"],
+        title   : json["title"],
+        value   : json["value"],
+        stock   : json["stock"],
+        photoUrl: json["photoUrl"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id"      : id,
+        "title"   : title,
+        "value"   : value,
+        "stock"   : stock,
+        "photoUrl": photoUrl,
+    };
+}
