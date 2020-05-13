00141e00584eea739381f5ab3e65501e1ece5dd2
german <heinz.german@gmail.com>
2020-05-13 15:24:26 +0200

    Form

--- lib/src/pages/product_page.dart
+++ lib/src/pages/product_page.dart
@@ -18,6 +18,47 @@ class ProductPage extends StatelessWidget {
             ),
         ],
       ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(15.0),
+        child: Container(
+          child: Form(
+            child: Column(children: <Widget>[
+              _createName(),
+              _createPrice(),
+              SizedBox(height: 20.0, width: 20.0),
+              _createButton(),
+            ],
+            )
+          ),
+        ),
+      ),
+    );
+  }
+  Widget _createName(){
+    return TextFormField(
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        labelText: 'Product'
+      ),
+    );
+  }
+
+  Widget _createPrice(){
+  return TextFormField(
+      keyboardType: TextInputType.numberWithOptions(decimal: true),
+      decoration: InputDecoration(
+        labelText: 'Price'
+      ),
+    );
+  }
+  Widget _createButton(){
+    return RaisedButton.icon(
+      label: Text('Save'),
+      icon: Icon(Icons.save),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      color: Colors.deepPurple,
+      textColor: Colors.white,
+      onPressed: (){},
     );
   }
 }
